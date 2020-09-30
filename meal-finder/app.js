495167055ced377a39d9d119e6940d8a9c4f7e9a
iDeveloperBrandon <hsy0602@outlook.com>
2020-09-30 16:52:20 +0900

    update with displayMeal

--- meal-finder/app.js
+++ meal-finder/app.js
@@ -1,25 +1,42 @@
-const formSubmit = document.getElementById("formSubmit");
+const formBox = document.getElementById("formSubmit");
 const inputBox = document.getElementById("inputBox");
-const searchTerm = document.getElementById("term");
-const result = document.querySelector(".result");
+const term = document.getElementById("term");
 
-// Event Listeners
-formSubmit.addEventListener("submit", searchMeal);
+const result = document.querySelector(".result");
+const meals = document.querySelector(".meals");
+
+// Event listener
+formBox.addEventListener("submit", listMeal);
 
 // functions
-function searchMeal(e) {
+function listMeal(e) {
   e.preventDefault();
 
-  const term = inputBox.value;
-  searchTerm.innerHTML = term;
+  let searchTerm = inputBox.value;
+  term.innerText = searchTerm;
+  inputBox.value = "";
+  if (searchTerm.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.meals);
 
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-    .then((res) => res.json())
-    .then(function (data) {
-      let imgEl = document.createElement("img");
-      let img = data.meals[0].strMealThumb;
-      imgEl.src = img;
-      imgEl.className = "result__img";
-      result.appendChild(imgEl);
-    });
+        if (data.meals === null) {
+          result.innerText = `There's no meal data in DB`;
+        } else {
+          meals.innerHTML = data.meals
+            .map(
+              (meal) => ` 
+          <div class="single--meal" id=${meal.idMeal}>
+          <img src="${meal.strMealThumb}"></img>
+            <h2>${meal.strMeal}</h2>
+          </div>
+          `
+            )
+            .join("");
+        }
+      });
+  } else {
+    alert("Type your fav food");
+  }
 }
