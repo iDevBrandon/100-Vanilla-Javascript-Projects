f9a7d30897dbcd097700f00488163b001ca95992
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-01 00:21:37 +0900

    single meal update

--- meal-finder/app.js
+++ meal-finder/app.js
@@ -3,9 +3,10 @@ const inputBox = document.getElementById("inputBox");
 const term = document.getElementById("term");
 
 const result = document.querySelector(".result");
-const meals = document.querySelector(".meals");
+const mealEl = document.querySelector(".meals");
 
 const randomBtn = document.getElementById("random");
+const singleEl = document.getElementById("single-meal");
 
 // Event listener
 formBox.addEventListener("submit", listMeal);
@@ -46,23 +47,71 @@ function listMeal(e) {
   }
 }
 
+// get single meal
+result.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((meal) => {
+    if (meal.classList) {
+      return meal.classList.contains("meal--info");
+    } else {
+      return false;
+    }
+  });
+  console.log(mealInfo);
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealid");
+    getMealById(mealID);
+  }
+});
+
+function getMealById(id) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const mealInfo = data.meals[0];
+      addMealToDom(mealInfo);
+    });
+}
+
+function addMealToDom(meal) {
+  const ingredients = [];
+  for (i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} -  ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleEl.innerHTML = `  
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+      </div>
+  `;
+}
+
+// display random meal
 function randomMeal() {
+  // clear heading
+  result.innerHTML = "";
+
   const url = "https://www.themealdb.com/api/json/v1/1/random.php";
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.meals);
-      result.innerHTML = data.meals
-        .map(
-          (meal) => ` 
-          <div class='single'>
-            <div class="single--meal" id=${meal.idMeal}>
-              <img src="${meal.strMealThumb}"></img>
-              <h2 class='single--heading'>${meal.strMeal}</h2>
-            </div>
-          </div>
-          `
-        )
-        .join("");
+      const meal = data.meals[0];
+      console.log(meal);
+      addMealToDom(meal);
     });
 }
