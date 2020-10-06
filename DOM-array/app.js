a10d8babf6bf2004e5b6435d2043eb5b29b07600
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-06 22:30:01 +0900

    high order array methods - forEach() map(),

---
+++ DOM-array/app.js
@@ -0,0 +1,55 @@
+const main = document.getElementById("main");
+const addBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+// initialize the data structure
+let data = [];
+
+// fetch random user
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  addData(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+}
+
+function updateDOM(providedData = data) {
+  main.innerHTML = "<h2><strong>Person</strong>Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> $${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// its from stackoverflow
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"); // 12,345.67
+}
+
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+addBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
