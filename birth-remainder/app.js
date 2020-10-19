1e34b3ea8ad61d94d8b7e22b1b6edd827f3d9822
iDeveloperBrandon <hsy0602@outlook.com>
2020-10-19 22:25:35 +0900

    complete

--- birth-remainder/app.js
+++ birth-remainder/app.js
@@ -1,1 +1,24 @@
-import data from "./data.js";
+import people from "./data.js";
+const peopleContainer = document.querySelector(".people-list");
+const btn = document.getElementById("clear-btn");
+
+peopleContainer.innerHTML = people
+  .map((person) => {
+    const { id, name, age, image } = person;
+    return `
+    <li id=${id}>
+      <img src=${image} alt=${name} />
+      <div>
+        <h4>${name}</h4>
+        <p>${age}</p>
+      </div>
+    </li>
+  `;
+  })
+  .join("");
+
+btn.addEventListener("click", () => clearList());
+
+function clearList() {
+  peopleContainer.innerHTML = "";
+}
