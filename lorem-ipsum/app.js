72447a5a4308584293cd780fae4e7c6de67d97ec
iDeveloperBrandon <hsy0602@outlook.com>
2020-11-25 14:17:20 +0900

    almost done without seperating

--- lorem-ipsum/app.js
+++ lorem-ipsum/app.js
@@ -1,17 +1,30 @@
 const btn = document.querySelector(".para__btn");
 const container = document.querySelector(".result");
+const amount = document.querySelector(".amount");
 
-// get the num from control part 
-
-// put that number in fetch()
-
+// get the num from control part
 const genParagraph = function (data) {
-  const html = `<p>${data}</p>`;
+  const html = `<p class='paragraph'>${data}</p>`;
   container.insertAdjacentHTML("beforeend", html);
 };
 
-btn.addEventListener("click", function (num) {
+// put that number in fetch()
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const num = amount.value;
+  console.log(num);
   fetch(`http://metaphorpsum.com/paragraphs/${num}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then(function (response) {
+      // The API call was successful!
+      return response.text();
+    })
+    .then(function (data) {
+      // This is the JSON from our response
+      console.log(data);
+      genParagraph(data);
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
 });
