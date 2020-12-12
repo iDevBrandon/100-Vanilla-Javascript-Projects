598d0438839bf8097d528606280dd2ebc27e7fee
Brandon Ha <hsy0602@outlook.com>
2020-12-12 16:23:10 +0900

    js 1st attempt

---
+++ 03-progress-steps/app.js
@@ -0,0 +1,9 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+const circle = document.querySelector(".circle");
+const progress = document.querySelector(".progress");
+
+prev.addEventListener("click", () => {
+  
+});
