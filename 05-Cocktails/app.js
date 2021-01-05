8fe82926beeb4c38c1a2e7b3f79a71db0a12cb83
Brandon Ha <hsy0602@outlook.com>
2021-01-05 14:17:59 +0900

    need a bit more JS study

--- 05-Cocktails/app.js
+++ 05-Cocktails/app.js
@@ -1,3 +1,19 @@
-//https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a
-
 const resultsContainer = document.querySelector(".results");
+const searchTerm = document.querySelector(".searchBox").value;
+
+console.log(searchTerm);
+
+const markup = `
+    <a class="result">
+        <img
+        src="https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg/preview"
+        alt=""
+        class="result__image"
+        />
+        <small class="result__name">Name</small>
+    </a>
+`;
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?i=sky")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
