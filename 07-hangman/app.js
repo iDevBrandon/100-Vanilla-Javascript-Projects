78be3fcb8671b4b163cff76ddde8f178f9f4c96f
Brandon Ha <hsy0602@outlook.com>
2021-01-05 22:33:11 +0900

    hangman

--- 07-hangman/app.js
+++ 07-hangman/app.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
+const playAgainBtn = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
@@ -50,9 +50,24 @@ function showNotifications() {
 
 function updateWrongLetters() {
   wrongLettersEl.innerHTML = `
-  ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
     ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
-  `;
+`;
+
+  figureParts.forEach((part, index) => {
+    const error = wrongLetters.length;
+
+    if (index < error) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost!";
+    popup.style.display = "flex";
+  }
 }
 
 // keydown letter press
@@ -76,6 +91,20 @@ window.addEventListener("keydown", (e) => {
       }
     }
   }
+});
+
+// restart button
+playAgainBtn.addEventListener("click", () => {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLetters();
+
+  popup.style.display = "none";
 });
 
 displayWord();
