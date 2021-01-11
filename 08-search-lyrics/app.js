37ef195298c399c680092efaa335adfec286d53f
Brandon Ha <hsy0602@outlook.com>
2021-01-12 07:06:33 +0900

    To get the search Term

---
+++ 08-search-lyrics/app.js
@@ -0,0 +1,10 @@
+const searchForm = document.querySelector(".hero--search--form");
+const searchInput = document.querySelector(".search--input");
+const searchBtn = document.querySelector(".search--button");
+const result = document.querySelector(".results");
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = searchInput.value;
+  
+});
