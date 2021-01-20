93cb25a76c21ebe231fe612cceac32d4bdb51185
iDevBrandon <40842018+iDevBrandon@users.noreply.github.com>
2021-01-20 18:27:45 +0900

    things to do

--- 09-movie-app/app.js
+++ 09-movie-app/app.js
@@ -1,5 +1,6 @@
 const searchForm = document.querySelector(".form");
 const searchInput = document.querySelector(".searchInput");
+const movieImg = document.querySelector(".movie--img");
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -9,4 +10,6 @@ searchForm.addEventListener("submit", (e) => {
   fetch(url)
     .then((response) => response.json())
     .then((data) => console.log(data));
+
+  movieImg.src = data.Search[0].Poster;
 });
