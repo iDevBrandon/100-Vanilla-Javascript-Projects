0b9f6881498c11ff9c8583765fafe42c1be383ea
iDevBrandon <40842018+iDevBrandon@users.noreply.github.com>
2021-01-21 20:30:00 +0900

    display each song lyrics

--- 08-search-lyrics/app.js
+++ 08-search-lyrics/app.js
@@ -58,6 +58,20 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = "";
+}
+
 // Event listeners
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -67,5 +81,17 @@ searchForm.addEventListener("submit", (e) => {
     alert("Type sth to search");
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// get lyrics button click
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
