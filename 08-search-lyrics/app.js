54829fa4830dbcc0444480ab0ee7a15693f3f143
iDevBrandon <40842018+iDevBrandon@users.noreply.github.com>
2021-01-21 11:40:13 +0900

    fetch songs & artist

--- 08-search-lyrics/app.js
+++ 08-search-lyrics/app.js
@@ -1,9 +1,25 @@
 const searchForm = document.querySelector(".hero--search--form");
 const searchInput = document.querySelector(".search--input");
-const searchBtn = document.querySelector(".search--button");
 const result = document.querySelector(".results");
 
+const apiURL = "https://api.lyrics.ovh";
+
+// Search by song or artist
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+  
+  console.log(data);
+}
+
+// Event listeners
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const searchTerm = searchInput.value;
+  const searchTerm = searchInput.value.trim();
+
+  if (!searchTerm) {
+    alert("Type sth to search");
+  } else {
+    searchSongs(searchTerm);
+  }
 });
