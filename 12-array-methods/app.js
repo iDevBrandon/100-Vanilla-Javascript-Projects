4334146cb9cc5c4058afb9706f958d9dee0cf152
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-02-08 17:34:09 +0900

    Generate Random Users - fetch/async/await

--- 12-array-methods/app.js
+++ 12-array-methods/app.js
@@ -1,1 +1,36 @@
-// https://randomuser.me/
+// DOM selections
+const main = document.querySelector(".main");
+const addUser = document.querySelector(".add-user");
+const doubleBtn = document.querySelector(".double-money");
+const showMillionaire = document.querySelector(".show-millionaire");
+const sortBtn = document.querySelector(".sort-btn");
+const sumBtn = document.querySelector(".sum-btn");
+
+let data = [];
+
+// fetch data from randomuser.me
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+// add new obj to HTML
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+
+}
