445e6e397d628c28b03b64c4c7c9f2ae12b255ba
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-07 16:06:31 +0900

    Rating

--- calendar/app.js
+++
@@ -1,9 +0,0 @@
-const calendar = document.querySelector("#app-calendar");
-
-const isWeekend = day => {
-    
-}
-
-for (let day = 1; day <= 31; day++) {
-  calendar.insertAdjacentHTML("beforeend", `<div class='day'>${day}</div>`);
-}
