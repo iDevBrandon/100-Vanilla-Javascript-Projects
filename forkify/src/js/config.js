c476b52dde8b88f2cfcc1e1b621eef457fe9c3e0
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-11 14:16:22 +0900

    promise.race

--- forkify/src/js/config.js
+++ forkify/src/js/config.js
@@ -3,3 +3,4 @@
 // allow us to configure by chaning some of the data
 
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
--- forkify/src/js/helpers.js
+++ forkify/src/js/helpers.js
@@ -1,9 +1,19 @@
 // The goal of this file is
 // to contain a couple of functions that we reuse
+import { TIMEOUT_SEC } from './config.js';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
 export const getJSON = async function (url) {
   try {
     // 1) Loading recipe
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
