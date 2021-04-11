f9d2abe04f807f5aad14a89228fd828146e7806c
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-04-12 03:51:33 +0900

    Display 10 results & set page number as 1(default)

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -6,9 +6,9 @@ import resultsView from './views/resultsView';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 ///////////////////////////////////////
 
@@ -40,7 +40,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results); // its calling ALL
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
