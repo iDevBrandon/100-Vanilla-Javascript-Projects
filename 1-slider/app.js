24dfeceb6c7c90da49113a6c039a8c6e7f45c473
Brandon Ha <hsy0602@outlook.com>
2021-04-27 02:31:46 +0900

    basic setup

--- 1-slider/app.js
+++ 1-slider/app.js
@@ -1,3 +1,14 @@
-console.log("====================================");
-console.log("hi");
-console.log("====================================");
+const slider = document.querySelector(".slider__container");
+
+const slide = document.querySelector(".slide");
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+function slideImg() {
+  console.log("123");
+}
+
+prevBtn.addEventListener("click", slideImg);
+
+nextBtn.addEventListener("click", slideImg);
