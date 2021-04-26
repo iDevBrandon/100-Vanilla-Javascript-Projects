b124fa6313caf5e0a450f7b4a1f24897a069e896
Brandon Ha <hsy0602@outlook.com>
2021-04-27 02:57:16 +0900

    re styling

--- 1-slider/app.js
+++ 1-slider/app.js
@@ -4,6 +4,7 @@ const slide = document.querySelector(".slide");
 
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
+const slideNum = document.querySelector(".number");
 
 function slideImg() {
   console.log("123");
