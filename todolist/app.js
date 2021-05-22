79d347ee3d670ce8bf5784b7ce9fc134bb70c672
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-22 21:05:32 +0900

    Filter todo part

--- todolist/app.js
+++ todolist/app.js
@@ -2,6 +2,7 @@
 const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todo-list");
 const todoButton = document.querySelector(".todo-button");
+const filterOption = document.querySelector(".filter-todo");
 
 // function
 const addTodo = (event) => {
@@ -56,6 +57,33 @@ const deleteCheck = (e) => {
   }
 };
 
+// Filter todo part
+const filterTodo = (e) => {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+};
+
 // Event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
