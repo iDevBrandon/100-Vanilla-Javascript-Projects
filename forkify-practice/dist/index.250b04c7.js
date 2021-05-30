597aff9a1fdb7b7a8c62e5852c2ae9f7c4387948
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-05-30 15:30:36 +0900

    Loading a Recipe from API

--- forkify-practice/dist/index.250b04c7.js
+++ forkify-practice/dist/index.250b04c7.js
@@ -455,6 +455,36 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// run first API Call
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886',
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc13'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // rename the property of data in API
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
 
 },{}]},["7BONy","3miIZ"], "3miIZ", "parcelRequirefade")
 
