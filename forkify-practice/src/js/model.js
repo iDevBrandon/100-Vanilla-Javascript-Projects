8cbd7be20eef4e880c8c656e16a57dc6a6d4410a
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-06-01 17:08:18 +0900

    stuck at private field stuff

---
+++ forkify-practice/src/js/model.js
@@ -0,0 +1,32 @@
+// When this state object here is going to get updated by loadRecipe
+export const state = {
+  recipe: {},
+};
+
+// fetching data from forkify API
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // rename the property of data in API
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
