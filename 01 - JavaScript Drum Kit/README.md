78205932907f263035dcb1fb4ec2939f484acac1
Brandon Ha <idevbrandon@gmail.com>
2021-08-05 17:18:56 +0900

    Wasbos JS 1

---
+++ 01 - JavaScript Drum Kit/README.md
@@ -0,0 +1,14 @@
+# Object
+
+In this project, we have to make it playable with the keyboard function.
+
+use
+
+<data-key='sth'> : Associate with the key code
+ add into class "key playing", "instead of just key".
+
+keycode.info as a ref web.
+keyCode is deprecated in today's time so use.
+
+`KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+<https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/code>
---
+++ 01 - JavaScript Drum Kit/app.js
@@ -0,0 +1,28 @@
+function playSong(e) {
+  const strKey = e.key;
+  const keyNum = strKey.charCodeAt();
+  const audio = document.querySelector(`audio[data-key="${keyNum}"]`);
+  if (!audio) {
+    return;
+  }
+
+  audio.play();
+
+  // rewind the audio play
+  audio.currentTime = 0;
+
+  // How to add animation
+  const key = document.querySelector(`.key[data-key="${keyNum}"]`);
+  key.classList.add("playing");
+}
+
+function endEvent(e) {
+  if (e.propertyName !== "transform") return;
+
+  this.classList.remove("playing");
+}
+
+// init
+window.addEventListener("keydown", playSong);
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", endEvent));
