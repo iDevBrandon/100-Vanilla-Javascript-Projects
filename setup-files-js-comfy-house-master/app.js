89a8126e49b6fb297dc480b60823362d314041d0
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-09-05 03:16:42 +0900

    getProducts with aclean array

--- setup-files-js-comfy-house-master/app.js
+++ setup-files-js-comfy-house-master/app.js
@@ -14,7 +14,24 @@ const productsDOM = document.querySelector(".products-section");
 let cart = [];
 
 // Getting the products class
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image }; // a clean object
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
 // Display products
 class UI {}
@@ -26,4 +43,7 @@ class Storage {}
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+
+  // get all products
+  products.getProducts().then((data) => console.log(data));
 });
