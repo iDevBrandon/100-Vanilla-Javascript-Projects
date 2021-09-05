5bd397e7e1ac1558319cff791ab86d03887f1bfc
Brandon Ha <40842018+iDevBrandon@users.noreply.github.com>
2021-09-06 07:31:13 +0900

    displayProducts in UI class

--- setup-files-js-comfy-house-master/app.js
+++ setup-files-js-comfy-house-master/app.js
@@ -8,7 +8,7 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-section");
+const productsDOM = document.querySelector(".products-center");
 
 // cart
 let cart = [];
@@ -34,7 +34,31 @@ class Products {
 }
 
 // Display products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((product) => {
+      result += `
+      <article class="product">
+        <div class="img-container">
+          <img
+            src=${product.image}
+            alt="product"
+            class="product-img"
+          />
+          <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            add to bag
+          </button>
+        </div>
+        <h3>${product.title}</h3>
+        <h4>$${product.price}</h4>
+      </article>
+      `;
+    });
+    productsDOM.innerHTML = result;
+  }
+}
 
 // local storage
 class Storage {}
@@ -45,5 +69,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then((data) => console.log(data));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
